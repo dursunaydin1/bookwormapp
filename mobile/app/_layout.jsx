0d9a0d0eaf592b21182606122878a9c5a1eca533
dursunaydin <dursunayydin@gmail.com>
2025-03-19 14:53:18 +0300

    Root Layout Setup

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SafeScreen from "../components/SafeScreen";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </SafeScreen>
+      <StatusBar style="dark" />
+    </SafeAreaView>
+  );
 }
--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -2,14 +2,23 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});

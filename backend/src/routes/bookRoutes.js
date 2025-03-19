159d683638785a0489e97584a23b2cc3ecfa1cdd
dursunaydin <dursunayydin@gmail.com>
2025-03-19 10:41:41 +0300

    Delete Book Route

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -62,4 +62,33 @@ router.get("/", protectRoute, async (req, res) => {
   }
 });
 
+// delete book
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+
+    // check if user is the owner
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // delete image from cloudinary
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.log("Error deleting image from Cloudinary:", deleteError);
+      }
+    }
+    await book.deleteOne();
+
+    res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting book", error);
+    res.status(500).json({ message: "Error deleting book" });
+  }
+});
+
 export default router;

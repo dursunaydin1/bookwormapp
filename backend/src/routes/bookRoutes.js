fdd7bff00ec964674bd22f4a2f497f2e6ee0cb34
dursunaydin <dursunayydin@gmail.com>
2025-03-19 10:22:33 +0300

    Get books with Pagination

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -4,39 +4,62 @@ import protectRoute from "../middlaware/auth.middleware.js";
 
 const router = express.Router();
 
-
 // create book
-router.post("/",protectRoute, async (req, res) => {
-    try {
-        const { title, caption, rating,image } = req.body;
-        
-        if (!title || !caption || !rating || !image) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
 
-        // upload image to cloudinary
-        const uploadResponse = await cloudinary.uploader.upload(image)
-        const imageUrl = uploadResponse.secure_url
-
-        // save to the database
-        const newBook = new Book({
-            title,
-            caption,
-            rating,
-            image: imageUrl,
-            user: req.user._id
-        })
-
-        await newBook.save();
-
-        res.status(201).json({ message: "Book created successfully" });
-    } catch (error) {
-        console.log("Error creating book", error);
-        res.status(500).json({ message: "Error creating book" });
+    if (!title || !caption || !rating || !image) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-})
 
+    // upload image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
 
+    // save to the database
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.user._id,
+    });
 
+    await newBook.save();
 
-export default router
+    res.status(201).json({ message: "Book created successfully" });
+  } catch (error) {
+    console.log("Error creating book", error);
+    res.status(500).json({ message: "Error creating book" });
+  }
+});
+
+// get all books paginated
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+
+    const totalBooks = await Book.countDocuments();
+
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+  } catch (error) {
+    console.log("Error getting books", error);
+    res.status(500).json({ message: "Error getting books" });
+  }
+});
+
+export default router;
